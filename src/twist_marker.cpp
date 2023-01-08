07a6198d5e46292c3e5bd6ae6ca8f3d0d58a0915
Bruce  L Digney <bdigney@shaw.ca>
2023-01-07 17:10:01 -0700

    first try

--- src/twist_marker.cpp
+++ src/twist_marker.cpp
@@ -9,7 +9,7 @@
  *
  *  To view a copy of this license, visit
  *  http://creativecommons.org/licenses/by-nc-sa/4.0/
- *  or send a letter to
+ *  or send a letter to  f
  *  Creative Commons, 444 Castro Street, Suite 900,
  *  Mountain View, California, 94041, USA.
  *********************************************************************/
